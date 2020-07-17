3cfade5899d393d36abb82ee14dec72ed2e1d2ea
ahy231 <zhongnanhai231@126.com>
2020-07-17 15:21:56 +0800

    add part7

---
+++ part7-Maven/workspace/consumerBanking/src/main/java/com/ahy231/bank/Util.java
@@ -0,0 +1,13 @@
+package com.ahy231.bank;
+/*
+ * @Copyright: free for non-commercial usage
+ * @Author: ahy231
+ * @Date: 2020-07-17 14:39:21
+ * @LastEditor: ahy231
+ * @LastEditTime: 2020-07-17 14:44:05
+ */ 
+public class Util {
+    public static void printMessage(String message){
+        System.out.println(message);
+    }
+}
